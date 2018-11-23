1d8a99cfa6572af438d955c9dc0968e47cf9541a
jrhenriquerf <jrhenriquerf@gmail.com>
2018-11-23 20:29:31 -0200

    Criação do Layout e implementação das models que serão utilizadas na activity principal

--- app/src/main/java/aula/fib/br/apionkotlin/APIOnKotlin.kt
+++ app/src/main/java/aula/fib/br/apionkotlin/APIOnKotlin.kt
@@ -1,7 +1,13 @@
 package aula.fib.br.apionkotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import aula.fib.br.apionkotlin.utils.Constants.API_KEY
+import aula.fib.br.apionkotlin.utils.Constants.URL_SERVIDOR
+import aula.fib.br.apionkotlin.utils.Util
+import org.jetbrains.anko.*
 
 class APIOnKotlin : AppCompatActivity() {
 
@@ -9,4 +15,9 @@ class APIOnKotlin : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_apion_kotlin)
     }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
 }
